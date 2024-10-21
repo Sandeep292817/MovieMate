55060567f2189dda082383d7d8a47bd50a9e2d9e
Sandeep@292817 <yelukurusandeep4a8@gmail.com>
2024-10-21 12:18:15 +0530

    updated Pagenotfound

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import PageError from '../assets/404NotFound.jpg';
 import { useTitle } from '../hooks/useTitle';
 import { Link } from 'react-router-dom';
